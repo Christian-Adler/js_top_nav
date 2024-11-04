1b4b077aacd38c53d9815cd400719187a2a20ff7
Christian Adler <chris.adler@gmx.de>
2024-11-04 15:41:15 +0100

    add scrollpos indicator

--- js/body-scroll.js
+++ js/body-scroll.js
@@ -7,24 +7,40 @@ const scrollDown = "scroll-down";
 
 let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll <= 0) {
-        body.classList.remove(scrollUp);
-        return;
-    }
+const scrollPosDiv = document.getElementById('scrollPos');
+let scrollPosVisibleTimer = null;
 
-    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        // down
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
-    } else if (
-        currentScroll < lastScroll &&
-        body.classList.contains(scrollDown)
-    ) {
-        // up
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
-    }
-    lastScroll = currentScroll;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+
+  let docHeight = document.body.offsetHeight;
+  let winHeight = window.innerHeight;
+  let scrollPercent = currentScroll / (docHeight - winHeight);
+  let scrollPercentRounded = Math.round(scrollPercent * 100);
+  scrollPosDiv.style.width = scrollPercentRounded + 'vw';
+  scrollPosDiv.classList.add('visible');
+
+  clearTimeout(scrollPosVisibleTimer);
+  scrollPosVisibleTimer = setInterval(() => {
+    scrollPosDiv.classList.remove('visible');
+  }, 800)
+
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (
+      currentScroll < lastScroll &&
+      body.classList.contains(scrollDown)
+  ) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
 });
